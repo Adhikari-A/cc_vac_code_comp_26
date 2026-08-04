9fd1b9738d5138198ef66874bbd4a69601c867b9
Ananya Adhikari <ananya.adhikari@tecnico.ulisboa.pt>
2026-08-05 00:51:03 +0100

    updated video of sub and sup evolutions

--- prep_for_push.sh
+++ prep_for_push.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-# This scripts preps before pushing to the remote repo
+# This script prepares the generated files before pushing.
 
 git status
 echo ============================
+
 rm -vrf index.html index-speaker.html index_files .quarto
+
 echo ============================
+
 quarto render index.qmd
+
 echo ============================
-git add index_files/libs/revealjs/dist/theme/quarto-*.css
+
+git add -A -- index.html index-speaker.html index_files
+
 echo ============================
+
 git status
